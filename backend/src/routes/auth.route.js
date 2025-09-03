b95d8f20d51433ac3f41c2a5878a711b7c81d67b
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-09-04 00:32:47 +0500

    add cors

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { Login, Logout, Profile, Signup } from '../controllers/auth.controller.js';
+import { protectRoutes } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/signup', Signup)
-router.get('/profile', Profile)
+router.get('/profile',protectRoutes, Profile)
 router.post('/login', Login);
 router.post('/logout', Logout)
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,20 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 import resumeRoutes from "./routes/resume.route.js"
+import cors from "cors"
 
 dotenv.config();
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Replace with your front-end URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials (like cookies or authorization headers)
+  optionsSuccessStatus: 204 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(cookieParser());
